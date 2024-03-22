b520cc21c1415d5fa46fa231547f0339957f063a
nguoihanoi <127606908+nguoihanoi@users.noreply.github.com>
2024-03-22 15:47:06 +0700

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SocketIO = void 0;
+const socket_io_1 = __importDefault(require("socket.io"));
+class SocketIO {
+    constructor(httpServer) {
+        this.io = socket_io_1.default(httpServer);
+    }
+    Server() {
+        return this.io;
+    }
+}
+exports.SocketIO = SocketIO;
+//# sourceMappingURL=index.js.map
