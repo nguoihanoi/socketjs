0373ef2853c57d8575f7f02201b9efed0aeb8209
nguoihanoi <127606908+nguoihanoi@users.noreply.github.com>
2024-03-22 16:15:04 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,3 @@
-import socketIO, { Server as SocketIOServer } from "socket.io";
-import {  Server as HTTPServer } from "http"; 
-
-export class SocketIO {
-  private io: SocketIOServer;  
-  constructor(httpServer: HTTPServer) { 
-    this.io = socketIO(httpServer);
-  } 
-  public Server() {
-    return this.io;
-  } 
+export function greet(name: string): string {
+  return `Hello ${name}`
 }
