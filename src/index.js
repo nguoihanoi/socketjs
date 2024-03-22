90e96ea98eeaf12a47a964fc1278d413bf713be3
nguoihanoi <127606908+nguoihanoi@users.noreply.github.com>
2024-03-22 15:47:52 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SocketIO = void 0;
-const socket_io_1 = __importDefault(require("socket.io"));
-class SocketIO {
-    constructor(httpServer) {
-        this.io = socket_io_1.default(httpServer);
-    }
-    Server() {
-        return this.io;
-    }
+import socketIO, { Server as SocketIOServer } from "socket.io";
+import {  Server as HTTPServer } from "http"; 
+
+export class SocketIO {
+  private io: SocketIOServer;  
+  constructor(httpServer: HTTPServer) { 
+    this.io = socketIO(httpServer);
+  } 
+  public Server() {
+    return this.io;
+  } 
 }
-exports.SocketIO = SocketIO;
-//# sourceMappingURL=index.js.map
